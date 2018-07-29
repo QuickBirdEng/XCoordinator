c6ccbaa1ed723c06216256701e938360e9544d66
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2018-07-30 00:49:14 +0200

    Reordering of files, using Animation for PageViewTransition

--- XCoordinator/Sources/Coordinator+PageView.swift
+++ XCoordinator/Sources/Coordinator+PageView.swift
@@ -7,6 +7,7 @@
 
 extension Coordinator where TransitionType == PageViewTransition {
     func set(_ viewControllers: [UIViewController], direction: UIPageViewControllerNavigationDirection, with options: TransitionOptions, animation: Animation?, completion: PresentationHandler?) {
+        rootViewController.transitioningDelegate = animation
         rootViewController.setViewControllers(viewControllers, direction: direction, animated: options.animated, completion: { _ in completion?() ?? () })
     }
 }
