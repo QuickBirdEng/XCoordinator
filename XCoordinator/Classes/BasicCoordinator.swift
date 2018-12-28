cdb71f77081d367b76d9e97db124180439861436
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2018-12-29 00:23:46 +0100

    Add documentation for BasicCoordinator

--- XCoordinator/Classes/BasicCoordinator.swift
+++ XCoordinator/Classes/BasicCoordinator.swift
@@ -10,10 +10,26 @@ public typealias BasicNavigationCoordinator<R: Route> = BasicCoordinator<R, Navi
 public typealias BasicViewCoordinator<R: Route> = BasicCoordinator<R, ViewTransition>
 public typealias BasicTabBarCoordinator<R: Route> = BasicCoordinator<R, TabBarTransition>
 
+///
+/// BasicCoordinator is a coordinator class that can be used without subclassing.
+///
+/// Although we encourage subclassing of coordinators for more complex cases, a `BaseCoordinator` can easily
+/// be created by only providing a `prepareTransition` closure, an `initialRoute` and an `initialLoadingType`.
+///
 open class BasicCoordinator<RouteType: Route, TransitionType: TransitionProtocol>: BaseCoordinator<RouteType, TransitionType> {
 
     // MARK: - Nested types
 
+    ///
+    /// `InitialLoadingType` differentiates between different points in time when the initital route is to
+    /// be triggered by the coordinator.
+    ///
+    /// - immediately:
+    ///     The initial route is triggered before the coordinator is made visible (i.e. on initialization).
+    ///
+    /// - presented:
+    ///     The initial route is triggered after the coordinator is made visible.
+    ///
     public enum InitialLoadingType {
         case immediately
         case presented
@@ -27,6 +43,21 @@ open class BasicCoordinator<RouteType: Route, TransitionType: TransitionProtocol
 
     // MARK: - Init
 
+    ///
+    /// Creates a BasicCoordinator.
+    ///
+    /// - Parameter initialRoute:
+    ///     The route to be triggered first.
+    ///     If you specify `nil`, no route is triggered - You can still trigger routes later on.
+    ///
+    /// - Parameter initialLoadingType:
+    ///     The point in time when the initial route is triggered.
+    ///     See `InitialLoadingType` for more information.
+    ///
+    /// - Parameter prepareTransition:
+    ///     A closure to define transitions based on triggered routes.
+    ///     Do not specify `nil` here, unless you are overriding `prepareTransition` by subclassing.
+    ///
     public init(initialRoute: RouteType? = nil,
                 initialLoadingType: InitialLoadingType = .presented,
                 prepareTransition: ((RouteType) -> TransitionType)?) {
