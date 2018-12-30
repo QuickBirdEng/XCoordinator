341b30aca932009397c231c7f09c34a821392a91
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2018-12-30 14:20:02 +0100

    Add documentation for SplitCoordinator

--- XCoordinator/Classes/SplitCoordinator.swift
+++ XCoordinator/Classes/SplitCoordinator.swift
@@ -6,10 +6,32 @@
 //  Copyright © 2018 QuickBird Studios. All rights reserved.
 //
 
+///
+/// SplitCoordinator can be used as a basis for a coordinator with a rootViewController of type
+/// `UISplitViewController`.
+///
+/// You can use all `SplitTransitions` and get an initializer to set a master and
+/// (optional) detail presentable.
+///
 open class SplitCoordinator<RouteType: Route>: BaseCoordinator<RouteType, SplitTransition> {
 
     // MARK: - Init
 
+    public override init(initialRoute: RouteType?) {
+        super.init(initialRoute: initialRoute)
+    }
+
+    ///
+    /// Creates a SplitCoordinator and sets the specified presentables as the rootViewController's
+    /// viewControllers.
+    ///
+    /// - Parameter master:
+    ///     The presentable to be shown as master in the `UISplitViewController`.
+    ///
+    /// - Parameter detail:
+    ///     The presentable to be shown as detail in the `UISplitViewController`. This is optional due to
+    ///     the fact that it might not be useful to have a detail page right away on a small-screen device.
+    ///
     public init(master: Presentable, detail: Presentable?) {
         super.init(initialTransition: .set([master, detail].compactMap { $0 }))
     }
