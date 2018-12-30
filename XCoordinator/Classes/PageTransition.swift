fee37038db8ea18ae305c60683e0577bfc66af54
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2018-12-30 14:02:26 +0100

    Add documentation to PageTransition

--- XCoordinator/Classes/PageTransition.swift
+++ XCoordinator/Classes/PageTransition.swift
@@ -9,6 +9,22 @@
 public typealias PageTransition = Transition<UIPageViewController>
 
 extension Transition where RootViewController: UIPageViewController {
+
+    ///
+    /// Sets the current page(s) of the rootViewController. Make sure to set
+    /// `UIPageViewController.isDoubleSided` to the appropriate setting before executing this transition.
+    ///
+    /// - Parameter first:
+    ///     The first page being shown. If second is specified as `nil`, this reflects a single page
+    ///     being shown.
+    ///
+    /// - Parameter second:
+    ///     The second page being shown. This page is optional, as your rootViewController can be used
+    ///     with `isDoubleSided` enabled or not.
+    ///
+    /// - Parameter direction:
+    ///     The direction in which the transition should be animated.
+    ///
     public static func set(_ first: Presentable, _ second: Presentable? = nil,
                            direction: UIPageViewController.NavigationDirection) -> PageTransition {
         let presentables = [first, second].compactMap { $0 }
