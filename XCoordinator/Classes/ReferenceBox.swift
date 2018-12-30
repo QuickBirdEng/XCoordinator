d11f20baa186fa8b7111e4473bee944106495dc2
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2018-12-30 14:12:12 +0100

    Add documentation to ReferenceBox

--- XCoordinator/Classes/ReferenceBox.swift
+++ XCoordinator/Classes/ReferenceBox.swift
@@ -6,6 +6,12 @@
 //  Copyright © 2018 QuickBird Studios. All rights reserved.
 //
 
+///
+/// A ReferenceBox can be used for any object that you need different retain strategies for.
+///
+/// For example, a ReferenceBox can be used to hold strongly to an object in the beginning,
+/// but later release this strong reference, but still hold the object weakly.
+///
 public class ReferenceBox<T: AnyObject> {
 
     // MARK: - Stored properties
@@ -15,6 +21,14 @@ public class ReferenceBox<T: AnyObject> {
 
     // MARK: - Init
 
+    ///
+    /// Creates a ReferenceBox with an optional start value.
+    ///
+    /// - Parameter value:
+    ///     The start value. If you specify `nil` here, no reference is set.
+    ///     You will later need to call `set` before you can use the object
+    ///     being referenced with the `get` method.
+    ///
     public init(_ value: T? = nil) {
         if let value = value {
             set(value)
@@ -23,15 +37,29 @@ public class ReferenceBox<T: AnyObject> {
 
     // MARK: - Methods
 
+    ///
+    /// Sets the strong and weak references to point to the specified object.
+    ///
+    /// - Parameter value:
+    ///     The object to be referenced.
+    ///
     public func set(_ value: T) {
         strongReference = value
         weakReference = value
     }
 
+    ///
+    /// This method can be used to retrieve the value set in the initializer or using the `set` method.
+    ///
     public func get() -> T? {
         return strongReference ?? weakReference
     }
 
+    ///
+    /// This method can be used to release the strong reference held to the object it is referencing to.
+    /// It will not remove the weak reference to the object and, therefore, as long as it is still held
+    /// strongly by another reference, you can access the value using the `get` method.
+    ///
     public func releaseStrongReference() {
         strongReference = nil
     }
