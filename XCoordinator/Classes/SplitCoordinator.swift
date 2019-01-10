892859ffdb71fb6e730fadf8efcae7191ee627f1
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2019-01-10 19:17:36 +0100

    Extract viewController-setting of SplitCoordinator into transition to ensure it is called after the window was created when using as window.rootViewController

--- XCoordinator/Classes/SplitCoordinator.swift
+++ XCoordinator/Classes/SplitCoordinator.swift
@@ -6,25 +6,11 @@
 //  Copyright © 2018 QuickBird Studios. All rights reserved.
 //
 
-public typealias SplitTransition = Transition<UISplitViewController>
-
 open class SplitCoordinator<RouteType: Route>: BaseCoordinator<RouteType, SplitTransition> {
 
     // MARK: - Init
 
     public init(master: Presentable, detail: Presentable?) {
-        super.init(initialRoute: nil)
-        setViewControllers([master, detail].compactMap { $0 })
-    }
-
-    // MARK: - Helpers
-
-    private func setViewControllers(_ presentables: [Presentable]) {
-        CATransaction.begin()
-        CATransaction.setCompletionBlock { [weak self] in
-            presentables.forEach { $0.presented(from: self) }
-        }
-        rootViewController.viewControllers = presentables.map { $0.viewController }
-        CATransaction.commit()
+        super.init(initialTransition: .set([master, detail].compactMap { $0 }))
     }
 }
