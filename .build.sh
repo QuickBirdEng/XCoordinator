044c83b12d5e250da9e15bd561672c72a5dd2b47
Paul Kraft <paul.kraft@tum.de>
2019-08-28 22:29:43 +0200

    Adapt build script to not include new lines

--- .build.sh
+++ .build.sh
@@ -1,5 +1,3 @@
 set -o pipefail
-swift build \
-	-Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" \
-	-Xswiftc "-target" -Xswiftc "x86_64-apple-ios13.0-simulator"
+swift build -Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios13.0-simulator"
 swift test
