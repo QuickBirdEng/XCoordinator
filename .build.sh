d5a69d6754371ffe3b4aa4aea54dfe27d5547d41
Paul Kraft <paul.kraft@tum.de>
2019-08-28 22:21:51 +0200

    Move build script into .build.sh file

---
+++ .build.sh
@@ -0,0 +1,5 @@
+set -o pipefail
+swift build \
+	-Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" \
+	-Xswiftc "-target" -Xswiftc "x86_64-apple-ios13.0-simulator"
+swift test
