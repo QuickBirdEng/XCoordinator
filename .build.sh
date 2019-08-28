ff48894af126bda3d678aa2cb3f813938171da08
Paul Kraft <paul.kraft@tum.de>
2019-08-28 23:20:50 +0200

    Add UIKit to .build.sh

--- .build.sh
+++ .build.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
 set -o pipefail
-swift build -Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios13.0-simulator"
-swift test -Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios13.0-simulator"
+swift build test \
+    -Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" \
+    -Xswiftc "-target" -Xswiftc "x86_64-apple-ios13.0-simulator" \
+    -Xswiftc "-lswiftUIKit"
