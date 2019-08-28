b75a496e93f494f829ad14901799764c0b4eeee5
Paul Kraft <pauljohanneskraft@icloud.com>
2019-08-28 16:31:21 +0200

    Specify that XCoordinator is only available on iOS

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "XCoordinator",
+    platforms: [.iOS(.v8)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
