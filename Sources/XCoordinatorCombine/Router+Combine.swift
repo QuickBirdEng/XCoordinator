790db27e2f781cd6e690815500cb563b62b00588
Paul Kraft <pauljohanneskraft@icloud.com>
2019-08-28 16:02:09 +0200

    Add additional targets for Combine & RxSwift

---
+++ Sources/XCoordinatorCombine/Router+Combine.swift
@@ -0,0 +1,23 @@
+//
+//  Router+Combine.swift
+//  XCoordinatorCombine
+//
+//  Created by Paul Kraft on 28.08.19.
+//  Copyright © 2018 QuickBird Studios. All rights reserved.
+//
+
+import Combine
+import XCoordinator
+
+@available(iOS 13.0, *)
+extension Router {
+    
+    public func triggerFuture(_ route: RouteType, with options: TransitionOptions = .init(animated: true)) -> Future<Void, Never> {
+        return Future { completion in
+            self.trigger(route, with: options) {
+                completion(.success(()))
+            }
+        }
+    }
+    
+}
---
+++ Sources/XCoordinatorRx/Router+Rx.swift
@@ -0,0 +1,61 @@
+//
+//  Router+Rx.swift
+//  XCoordinatorRx
+//
+//  Created by Paul Kraft on 28.08.19.
+//  Copyright © 2018 QuickBird Studios. All rights reserved.
+//
+
+import XCoordinator
+import RxSwift
+
+extension Router {
+
+    /// Use this to access the reactive extensions of `Router` objects.
+    public var rx: Reactive<Self> {
+        // swiftlint:disable:previous identifier_name
+        return Reactive(self)
+    }
+}
+
+extension Reactive where Base: Router & AnyObject {
+
+    ///
+    /// This method transforms the completion block of a router's trigger method into an observable.
+    ///
+    /// - Parameter route:
+    ///     The route to be triggered.
+    ///
+    /// - Parameter options:
+    ///     Transition options, e.g. defining whether or not the transition should be animated.
+    ///
+    /// - Returns:
+    ///     An observable informing about the completion of the transition.
+    ///
+    public func trigger(_ route: Base.RouteType, with options: TransitionOptions) -> Observable<Void> {
+        return Observable.create { [base] observer -> Disposable in
+            base.trigger(route, with: options) {
+                observer.onNext(())
+                observer.onCompleted()
+            }
+            return Disposables.create()
+        }
+    }
+
+    // MARK: - Convenience methods
+
+    ///
+    /// This method transforms the completion block of a router's trigger method into an observable.
+    ///
+    /// It uses the default transition options as specified in `Router.trigger`.
+    ///
+    /// - Parameter route:
+    ///     The route to be triggered.
+    ///
+    /// - Returns:
+    ///     An observable informing about the completion of the transition.
+    ///
+    public func trigger(_ route: Base.RouteType) -> Observable<Void> {
+        return trigger(route, with: TransitionOptions(animated: true))
+    }
+}
