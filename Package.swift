08478737551c74604d45c7c75670ee5516c45499
Paul Kraft <pauljohanneskraft@icloud.com>
2019-08-28 16:02:09 +0200

    Add additional targets for Combine & RxSwift

--- Package.swift
+++ Package.swift
@@ -10,10 +10,17 @@ let package = Package(
         .library(
             name: "XCoordinator",
             targets: ["XCoordinator"]),
+        .library(
+            name: "XCoordinatorRx",
+            targets: ["XCoordinatorRx"]),
+        .library(
+            name: "XCoordinatorCombine",
+            targets: ["XCoordinatorCombine"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -21,6 +28,12 @@ let package = Package(
         .target(
             name: "XCoordinator",
             dependencies: []),
+        .target(
+            name: "XCoordinatorRx",
+            dependencies: ["XCoordinator", "RxSwift"]),
+        .target(
+            name: "XCoordinatorCombine",
+            dependencies: ["XCoordinator"]),
         .testTarget(
             name: "XCoordinatorTests",
             dependencies: ["XCoordinator"]),
