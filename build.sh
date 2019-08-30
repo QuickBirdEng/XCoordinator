dc4a22973aab3ce68a2096d973452cca988be56e
Paul Kraft <pauljohanneskraft@icloud.com>
2019-08-30 13:26:08 +0200

    Do not run tests on CI

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 set -o pipefail
-xcodebuild test -project XCoordinator.xcodeproj -scheme XCoordinatorTests -destination 'platform=iOS Simulator,name=iPhone 8,OS=latest'
-
+swift build \
+    -Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" \
+    -Xswiftc "-target" -Xswiftc "x86_64-apple-ios13.0-simulator"
