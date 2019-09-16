59089da456547b745c841a55a96d0fd4266d90fe
Paul Kraft <paul.kraft@tum.de>
2019-09-16 21:34:47 +0200

    Cleanup CI scripts

--- scripts/check_docs.sh
+++ scripts/check_docs.sh
@@ -1,4 +1,12 @@
+
+# Preparation
+
+cd "$( dirname "$0" )"
+
+# Execution
+
 ./docs.sh
+
 if [ -z "$(git status --untracked-files=no --porcelain)" ]; then 
    exit 0
 else
--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,5 +1,20 @@
-./build.sh
+#!/bin/sh
+
+# Preparation
+
+cd "$( dirname "$0" )"
+
+# Constants
+
 jazzy_file_url="tmp_jazzy.json"
+
+# Execution
+
+./build.sh
+cd ..
 sourcekitten doc --spm-module XCoordinator > $jazzy_file_url
 jazzy --sourcekitten-sourcefile $jazzy_file_url
+
+# Cleanup
+
 rm $jazzy_file_url
