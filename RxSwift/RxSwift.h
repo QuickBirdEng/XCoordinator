1d097c1fc964971a72bc43f4f4c6a257617252ec
An Tran <tran.binhan@gmail.com>
2020-05-06 21:10:21 +0200

    Fix

---
+++ RxSwift/RxSwift.h
@@ -0,0 +1,18 @@
+//
+//  RxSwift.h
+//  RxSwift
+//
+//  Created by An Tran on 06.05.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxSwift.
+FOUNDATION_EXPORT double RxSwiftVersionNumber;
+
+//! Project version string for RxSwift.
+FOUNDATION_EXPORT const unsigned char RxSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSwift/PublicHeader.h>
+
+
