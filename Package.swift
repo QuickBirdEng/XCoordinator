ada6f65e65af91ce494fb3a133e4e6b2799bbebb
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2021-04-16 21:09:45 +0200

    Update RxSwift to 6.1.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "XCoordinator",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.1"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
