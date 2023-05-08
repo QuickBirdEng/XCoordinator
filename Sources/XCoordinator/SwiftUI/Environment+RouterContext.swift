b63c15e9ae844201ef4febdee84c2be4ef5d2020
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2023-05-08 16:38:15 +0200

    Add Routing property wrapper

--- Sources/XCoordinator/SwiftUI/Environment+RouterContext.swift
+++ Sources/XCoordinator/SwiftUI/Environment+RouterContext.swift
@@ -12,15 +12,39 @@ import SwiftUI
 
 @available(iOS 13.0, tvOS 13.0, *)
 private enum RouterContextKey: EnvironmentKey {
+
     static var defaultValue: RouterContext { .init() }
+
 }
 
 @available(iOS 13.0, tvOS 13.0, *)
 extension EnvironmentValues {
+
     public var router: RouterContext {
         get { self[RouterContextKey.self] }
         set { self[RouterContextKey.self] = newValue }
     }
+
+}
+
+@available(iOS 13.0, tvOS 13.0, *)
+@propertyWrapper
+public struct Routing<RouteType: Route>: DynamicProperty {
+
+    // MARK: Stored Properties
+
+    @Environment(\.router) private var context
+
+    // MARK: Computed Properties
+
+    public var wrappedValue: (any Router<RouteType>)? {
+        context.router(for: RouteType.self)
+    }
+
+    // MARK: Initialization
+
+    public init(_ routeType: RouteType.Type) {}
+
 }
 
 #endif
